a8b965c4068880737482f333443f3188c28009a1
Suray Marag Pandit <118465425+suray-marag-pandit@users.noreply.github.com>
2024-04-30 09:48:10 +0530

    list

--- main.py
+++ main.py
@@ -52,3 +52,45 @@ assigned to the variable"""
 # Boolean Type:	bool
 # Binary Types:	bytes, bytearray, memoryview
 # None Type:	NoneType
+
+
+        # There are four collection data types in the Python programming language:
+
+                # List is a collection which is ordered and changeable. Allows duplicate members.
+                # Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+                # Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+                # Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+
+        #LISTS
+
+        # appened , insert , extend
+        # remove pop del clear
+        # in for while comprehension
+        #List comprehension
+        # list.sort()
+        # list.sort(reverse = true) // descending order
+        # list.reverse()  //reverse the original list
+        # newlist = list.copy()
+        # newlist = list(thislist)
+        # newlist = list1+ list2
+
+
+        # append()	Adds an element at the end of the list
+        # clear()	Removes all the elements from the list
+        # copy()	Returns a copy of the list
+        # count()	Returns the number of elements with the specified value
+        # extend()	Add the elements of a list (or any iterable), to the end of the current list
+        # index()	Returns the index of the first element with the specified value
+        # insert()	Adds an element at the specified position
+        # pop()	Removes the element at the specified position
+        # remove()	Removes the item with the specified value
+        # reverse()	Reverses the order of the list
+        # sort()	Sorts the list
+
+
+#tuples
+
+        # Tuple items are ordered, unchangeable, and allow duplicate values.
+        # len(thistuple)
+
---
+++ trial/list.py
@@ -0,0 +1,21 @@
+# l = [1,2,3,4]
+# print(l)
+
+
+# fruits = ["apple", "banana", "cherry", "mango","kiwi"]
+# fruits.sort()
+# print(fruits)
+# newlist = [s[0] for s in fruits if 'a' in s]
+
+# print(newlist)
+
+# i = [x for x in range(10)]
+# print(i)
+
+# print([x if x != "apple" else "orange" for x in fruits])
+
+# l = [x for x in range(100) if x%2==0 ]
+# l = [x if x%2==0 else 'x' for x in range(10)]
+# newlist = [expression for item in iterable if condition == True]
+l = [ x  for x in range(100) if x%2==0 ]
+print(l)
